41d542c4e6a289d1c0dcff4ac49fe065737da505
Zaturrby <rjkorteschiel@gmail.com>
2015-03-14 20:06:25 +0100

    added data and some things to mysings to mysql test #13

---
+++ mysql-test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     	: 'localhost',
+  user		: 'root',
+  password	: ''
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+
+// brew install mysql
+// mysql.server start
+// mysql -u 'root' -p < src/data/dump_bgb.sql
