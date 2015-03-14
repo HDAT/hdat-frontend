180fe84352ad3cef2a7463e35ee0efdb342c2d8b
Zaturrby <rjkorteschiel@gmail.com>
2015-03-15 00:02:28 +0100

    #13 postgres queries work

---
+++ src/scripts/build/postgresql-test.js
@@ -0,0 +1,25 @@
+// To run this script:
+// node src/scripts/build/postgesql-test.js
+
+'use strict';
+
+var pg = require('pg');
+var conString = "postgres://Robert-Jan:postgres@localhost/bgb";
+var query = 'SELECT * FROM "bgbProduct"';
+
+pg.connect(conString, function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query(query, function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result);
+    //output: 1
+    client.end();
+  });
+});
