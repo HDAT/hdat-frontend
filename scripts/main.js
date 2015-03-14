de6e482b71df601167d14c7f4e04803b1f50080d
Zaturrby <rjkorteschiel@gmail.com>
2015-03-14 19:01:42 +0100

    Update 2015-03-14T18:01:40.678Z

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@
 	var map = L.map('map').setView([0, 0], 3);
 	L.tileLayer('images/tiles/{z}/{x}/{y}.png', {
 	    minZoom: 3,
-	    maxZoom: 6,
+	    maxZoom: 5,
 	    attributionControl: false,
 	    tms: true,
 	    noWrap: true
