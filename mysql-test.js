04f7b7a538e5949e145f4198ca7ab3a5372c1b66
Zaturrby <rjkorteschiel@gmail.com>
2015-03-14 19:31:49 +0100

    #13 created simple node interface with mysql server

---
+++ mysql-test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+
+
+// brew install mysql
+// mysql.server start
