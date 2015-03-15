639426a5458975f10673965fc6f069b642661d10
Zaturrby <rjkorteschiel@gmail.com>
2015-03-15 10:46:50 +0100

    #13 small change to postsql-test

--- src/scripts/build/postgresql-test.js
+++ src/scripts/build/postgresql-test.js
@@ -5,8 +5,11 @@
 
 var pg = require('pg');
 var conString = "postgres://root:secret@localhost/bgb";
+
+// You can type a single query here. Chaining multiple is more interesting however.
 var query = 'SELECT * FROM "bgbProduct"';
 
+//The code that does the connecting to the DB
 pg.connect(conString, function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
