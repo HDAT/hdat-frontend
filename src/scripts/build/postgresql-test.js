eb39dda738d52231eedfd11c750f865201dd72be
Zaturrby <rjkorteschiel@gmail.com>
2015-03-19 12:18:34 +0100

    #9 node script loop

--- src/scripts/build/postgresql-test.js
+++ src/scripts/build/postgresql-test.js
@@ -7,25 +7,32 @@ var pg = require('pg');
 var conString = "postgres://root:secret@localhost/bgb";
 
 // You can type a single query here. Chaining multiple is more interesting however.
-var query = 'SELECT * FROM "bgbPlace"';
+var query = [
+  'SELECT * FROM "bgbPlace"',
+  'SELECT * FROM "bgbCargo"'
+];
+
 
 //The code that does the connecting to the DB
 pg.connect(conString, function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-  client.query(query, function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
-    if(err) {
-      return console.error('error running query', err);
-    }
 
-    // Logging out results
-    result.rows.forEach(function(value, index, array){
-    	console.log(value.naam);
+  query.forEach(function(ArrValue, Arrkey, ArrWhatever){
+    client.query(ArrValue, function(err, result) {
+      //call `done()` to release the client back to the pool
+      done();
+      if(err) {
+        return console.error('error running query', err);
+      }
+
+      // Logging out results
+      result.rows.forEach(function(value, index, array){
+      	console.log(value);
+      });
     });
-
-    client.end();
   });
+
+  client.end();
 });
