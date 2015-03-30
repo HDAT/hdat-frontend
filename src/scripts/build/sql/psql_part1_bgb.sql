81d9494b152dfc22a19ce3e3f9ff5c229153d17b
Zaturrby <rjkorteschiel@gmail.com>
2015-03-30 15:06:00 +0200

    created start on postgres

---
+++ src/scripts/build/sql/psql_part1_bgb.sql
@@ -0,0 +1,22 @@
+-- Create table for use by Torque
+
+DROP TABLE IF EXISTS "allVoyagePoints";
+CREATE TABLE "allVoyagePoints" (
+	id SERIAL PRIMARY KEY,
+	voyId integer,
+	lat float,
+	lng float,
+	voyArrTimeStamp timeStamp,
+	voyDepTimeStamp timeStamp
+);
+
+-- Selecting the required data
+
+INSERT INTO "allVoyagePoints" (voyId, voyArrTimeStamp, voyDepTimeStamp)
+SELECT 
+	"voyId",
+	CASE WHEN "voyArrivalYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyArrivalDay", "voyArrivalMonth", "voyArrivalYear"),  'DD MM YYYY')
+	END,
+	CASE WHEN "voyDepartureYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyDepartureDay", "voyDepartureMonth", "voyDepartureYear"),  'DD MM YYYY')
+	END
+FROM "bgbVoyage";
