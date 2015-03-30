999f231540ef530e0b4de42c1a78ec69be5a4f46
Zaturrby <rjkorteschiel@gmail.com>
2015-03-30 20:22:55 +0200

    with updata to 1

--- src/scripts/build/sql/torqueTable.sql
+++ src/scripts/build/sql/torqueTable.sql
@@ -2,24 +2,37 @@
 
 DROP TABLE IF EXISTS "allVoyagePoints";
 CREATE TABLE "allVoyagePoints" (
-	id SERIAL PRIMARY KEY,
-	voyId integer,
-	lat float,
-	lng float,
-	voyArrTimeStamp timeStamp,
-	voyDepTimeStamp timeStamp
+	"id" SERIAL PRIMARY KEY,
+	"voyId" integer,
+	"voyArrivalPlaceId" integer,
+	"voyDeparturePlaceId" integer,
+	"voyArrivalPlaceCoord" float,
+	"voyDeparturePlaceCoord" float,
+	"voyArrTimeStamp" timeStamp,
+	"voyDepTimeStamp" timeStamp
 );
 
 -- Selecting the required data
 
-INSERT INTO "allVoyagePoints" (voyId, lat, lng, voyArrTimeStamp, voyDepTimeStamp)
+INSERT INTO "allVoyagePoints" ("voyId", "voyDeparturePlaceId", "voyArrivalPlaceId", "voyArrTimeStamp", "voyDepTimeStamp")
 SELECT 
 	"voyId",
-	geo.lat,
-	geo.lng,
+	"voyDeparturePlaceId",
+	"voyArrivalPlaceId",
 	CASE WHEN "voyArrivalYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyArrivalDay", "voyArrivalMonth", "voyArrivalYear"),  'DD MM YYYY')
 	END,
 	CASE WHEN "voyDepartureYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyDepartureDay", "voyDepartureMonth", "voyDepartureYear"),  'DD MM YYYY')
 	END
-FROM "bgbVoyage" voyage, "bgbPlaceGeo" geo
-WHERE "voyDeparturePlaceId" = geo.id;
+FROM "bgbVoyage";
+
+-- Converting places to points
+
+UPDATE "allVoyagePoints"
+SET "voyDeparturePlaceCoord" = '1';
+-- FROM "bgbPlaceGeo" geo
+-- WHERE "voyDeparturePlaceId" = geo.id; 
+
+
+
+
+-- ST_SetSRID(ST_MakeLine(ST_MakePoint(geo.lat, geo.lng), ST_MakePoint(geo.lat, geo.lng)),4326)
