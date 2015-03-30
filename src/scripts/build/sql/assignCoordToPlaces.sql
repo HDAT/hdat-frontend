d477e96793cefea29c0866e24c97ae3c51d7740d
Zaturrby <rjkorteschiel@gmail.com>
2015-03-30 18:39:55 +0200

    with to_number

--- src/scripts/build/sql/assignCoordToPlaces.sql
+++ src/scripts/build/sql/assignCoordToPlaces.sql
@@ -15,11 +15,13 @@ COPY "placesAtlas" FROM '/Users/Robert-Jan/Desktop/HDAT/src/data/geodataAtlas.ht
 DROP TABLE IF EXISTS "bgbPlaceGeo";
 CREATE TABLE "bgbPlaceGeo" AS
   TABLE "bgbPlace";
-ALTER TABLE "bgbPlaceGeo" ADD COLUMN "coordinate" varchar;
-
+ALTER TABLE "bgbPlaceGeo" ADD COLUMN "lng" float;
+ALTER TABLE "bgbPlaceGeo" ADD COLUMN "lat" float;
 -- Patternmatch places
 
 UPDATE "bgbPlaceGeo"
-SET coordinate = coordinated
+SET 
+	lng = to_number(split_part('-3.55,128.3', ',', 1),'MI9.9'),
+	lat = to_number(split_part('-3.55,128.3', ',', 2),'MI9.9')
 FROM "placesAtlas"
 WHERE naam = place;
