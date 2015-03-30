d5d37c193b44b6aaa74b259d41c1b29c1dc76a56
Zaturrby <rjkorteschiel@gmail.com>
2015-03-30 19:56:53 +0200

    withdep torque

--- src/scripts/build/sql/torqueTable.sql
+++ src/scripts/build/sql/torqueTable.sql
@@ -15,13 +15,11 @@ CREATE TABLE "allVoyagePoints" (
 INSERT INTO "allVoyagePoints" (voyId, lat, lng, voyArrTimeStamp, voyDepTimeStamp)
 SELECT 
 	"voyId",
-	"lat",
-	"lng",
+	geo.lat,
+	geo.lng,
 	CASE WHEN "voyArrivalYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyArrivalDay", "voyArrivalMonth", "voyArrivalYear"),  'DD MM YYYY')
 	END,
 	CASE WHEN "voyDepartureYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyDepartureDay", "voyDepartureMonth", "voyDepartureYear"),  'DD MM YYYY')
 	END
-FROM "bgbVoyage", "bgbPlaceGeo";
-
-
-
+FROM "bgbVoyage" voyage, "bgbPlaceGeo" geo
+WHERE "voyDeparturePlaceId" = geo.id;
