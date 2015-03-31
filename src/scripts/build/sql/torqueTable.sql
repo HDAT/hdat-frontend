71e353ec805e57fb9e7a023ad0d9faa7833de741
evanzummeren <erikvanzummeren@gmail.com>
2015-04-01 00:31:41 +0200

    final tab fx

--- src/scripts/build/sql/torqueTable.sql
+++ src/scripts/build/sql/torqueTable.sql
@@ -63,9 +63,9 @@ CREATE OR REPLACE FUNCTION insertPoints(integer, geometry, timestamp, timestamp)
 $$
 DECLARE
    	iterator 	float 	:= 1; 
-   	steps    	float	:= round(((ST_Length_Spheroid($2,'SPHEROID["WGS 84",6378137,298.257223563]'))/1000)/50); -- iedere 50 km een stap
+   	steps    	float		:= round(((ST_Length_Spheroid($2,'SPHEROID["WGS 84",6378137,298.257223563]'))/1000)/50); -- iedere 50 km een stap
    	speed			integer	:= 10; -- km/h
-	increment		float 	:= 0;
+		increment		float := 0;
 
 BEGIN
    	WHILE iterator < steps
@@ -82,9 +82,9 @@ BEGIN
       VALUES (
       	$1,
       	ST_Line_Interpolate_Point($2, iterator/steps),
-      	CASE WHEN $3 IS NOT NULL THEN $3 + interval '1h' * increment
-				WHEN $4 IS NOT NULL THEN $4 - interval '1h' * increment
-		END
+      	CASE 	WHEN $3 IS NOT NULL THEN $3 + interval '1h' * increment
+							WHEN $4 IS NOT NULL THEN $4 - interval '1h' * increment
+				END
       );
 
       iterator := iterator + 1;
