7eb7e8c6cca9c9eb7651237ce9fc955b24680bad
Zaturrby <rjkorteschiel@gmail.com>
2015-04-10 14:32:12 +0200

    change

--- src/scripts/build/sql/assignCoordToPlaces.sql
+++ src/scripts/build/sql/assignCoordToPlaces.sql
@@ -1,14 +1,16 @@
 -- Assign Coordinates to places in bgbPlaces
 
--- Load amh_locaties.csv
+-- Load amh_location.csv
 
-DROP TABLE IF EXISTS "placesAtlas";
-CREATE TABLE "placesAtlas" (
-  place varchar(255),
-  coordinate varchar(255)
+DROP TABLE IF EXISTS "amhPlaces";
+CREATE TABLE "amhPlaces" (
+	title varchar(255),
+	record_type varchar(255),
+	latitude varchar(255),
+	longitude varchar(255)
 );
 
-COPY "placesAtlas" FROM '/Users/Erik/Desktop/HDAT/src/data/geodataAtlas.html' DELIMITER ',' CSV;
+COPY "amhPlaces" FROM '/Users/Robert-Jan/Desktop/HDAT/src/data/amh_location.csv' DELIMITER ',' CSV;
 
 -- Create table bgbPlaceGeo
 
@@ -22,10 +24,10 @@ ALTER TABLE "bgbPlaceGeo" ADD COLUMN "lng" float;
 
 UPDATE "bgbPlaceGeo"
 SET 
-	lat = CAST(split_part("coordinate", ',', 1) as FLOAT),
-	lng = CAST(split_part("coordinate", ',', 2) as FLOAT)
-FROM "placesAtlas"
-WHERE naam = place;
+	lat = latitude,
+	lng = longitude
+FROM "amhPlaces"
+WHERE naam = title;
 
 
 
