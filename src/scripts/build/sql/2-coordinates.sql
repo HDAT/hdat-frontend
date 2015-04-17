f3642f0d75ab4ac2ccee788b195e18ed9cb0897d
evanzummeren <erikvanzummeren@gmail.com>
2015-04-17 17:35:52 +0200

    fix last thing

--- src/scripts/build/sql/2-coordinates.sql
+++ src/scripts/build/sql/2-coordinates.sql
@@ -1,17 +1,3 @@
--- Assign Coordinates to places in bgbPlaces
-
--- Load amh_location.csv
-
-DROP TABLE IF EXISTS "amhPlaces";
-CREATE TABLE "amhPlaces" (
-	title varchar(255),
-	record_type varchar(255),
-	latitude varchar(255),
-	longitude varchar(255)
-);
-
-COPY "amhPlaces" FROM '/Users/Erik/Desktop/HDAT/src/data/amh_location_mod.csv' DELIMITER ',' CSV;
-
 -- PLACES
 -- Setup
 
