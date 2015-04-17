2cfc908b3cc8224f88e65e8690545b8dc810be73
evanzummeren <erikvanzummeren@gmail.com>
2015-04-17 20:51:29 +0200

    mapfix

--- src/scripts/build/sql/1-routing.sql
+++ src/scripts/build/sql/1-routing.sql
@@ -5,9 +5,7 @@ ALTER TABLE "routingMod"
     ADD COLUMN source integer, 
     ADD COLUMN target integer,
     ADD COLUMN cost double precision, 
-    ADD COLUMN rcost double precision,
-    ADD PRIMARY KEY (gid);
-
+    ADD COLUMN rcost double precision;
 
 -- Creation of the nodes from the linesegments
 SELECT pgr_createTopology('routingMod', 0.000001, 'geom', 'gid');
@@ -16,9 +14,3 @@ SELECT pgr_createTopology('routingMod', 0.000001, 'geom', 'gid');
 UPDATE "routingMod" SET 
     cost = ST_Length_Spheroid(geom,'SPHEROID["WGS84",6378137,298.257223563]'), 
     rcost = ST_Length_Spheroid(geom,'SPHEROID["WGS84",6378137,298.257223563]');
-
-DELETE FROM "routingMod"
-WHERE gid = 43;
-
-DELETE FROM "routingMod"
-WHERE gid = 44;
