aefaece49510ca928e2795a52c615e6df2611d36
Zaturrby <rjkorteschiel@gmail.com>
2015-04-17 19:24:01 +0200

    change

--- src/scripts/build/sql/4-voyagesTime.sql
+++ src/scripts/build/sql/4-voyagesTime.sql
@@ -1,10 +1,10 @@
 -- TEMPORAL CONCATENATION
 
-	-- CASE 
-	-- 	WHEN "voyArrivalYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyArrivalDay", "voyArrivalMonth", "voyArrivalYear"),  'DD MM YYYY')
-	-- END,
-	-- CASE 
-	-- 	WHEN "voyDepartureYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyDepartureDay", "voyDepartureMonth", "voyDepartureYear"),  'DD MM YYYY')
-	-- END
+-- 	CASE 
+-- 		WHEN "voyArrivalYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyArrivalDay", "voyArrivalMonth", "voyArrivalYear"),  'DD MM YYYY')
+-- 	END,
+-- 	CASE 
+-- 		WHEN "voyDepartureYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyDepartureDay", "voyDepartureMonth", "voyDepartureYear"),  'DD MM YYYY')
+-- 	END
 
 
