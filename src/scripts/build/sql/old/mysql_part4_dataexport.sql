f0d222f3bf1839c913602dc8e0950a5b793f9b69
Zaturrby <rjkorteschiel@gmail.com>
2015-04-17 15:05:31 +0200

    cleanup

--- src/scripts/build/sql/old/mysql_part4_dataexport.sql
+++
@@ -1,4 +0,0 @@
-SELECT * INTO OUTFILE '/Users/erik/Desktop/hdat/data/mysqlexport.csv'
-    FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-    LINES TERMINATED BY '\n'
-FROM bgbdasVoyageMergeRouteId;
