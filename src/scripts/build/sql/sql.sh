90794565a9c99dbdeffb11b622947c5ca333885a
evanzummeren <erikvanzummeren@gmail.com>
2015-04-17 19:23:45 +0200

    final shell

--- src/scripts/build/sql/sql.sh
+++ src/scripts/build/sql/sql.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Import map"
+shp2pgsql -c -D -s 4326 -I /Users/$USER/Desktop/HDAT/src/data/map/map.shp routing | psql bgb
+
 echo "Create functions"
 psql -d bgb -f functions/determineRoute.sql
 psql -d bgb -f functions/findNearestNode.sql
@@ -12,5 +15,5 @@ psql -d bgb -c "COPY \"amhPlaces\" FROM '/Users/$USER/Desktop/HDAT/src/data/amh_
 echo "1,2,3"
 psql -d bgb -f 1-routing.sql
 psql -d bgb -f 2-coordinates.sql
-psql -d bgb -f 3-voyages.sql
+psql -d bgb -f 3-voyagesGeo.sql
 psql -d bgb -f 4-voyagesTime.sql
