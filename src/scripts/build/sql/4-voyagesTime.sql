88bc45deac8dc796435498fd0a8adcce30f6ce96
evanzummeren <erikvanzummeren@gmail.com>
2015-04-18 15:30:09 +0200

    check op dag en maand

--- src/scripts/build/sql/4-voyagesTime.sql
+++ src/scripts/build/sql/4-voyagesTime.sql
@@ -2,6 +2,11 @@ ALTER TABLE "bgbVoyageRoute"
 	ADD COLUMN "voyDepTimeStamp" timeStamp,
 	ADD COLUMN "voyArrTimeStamp" timeStamp;
 
+UPDATE "bgbVoyageRoute"
+SET "voyDepTimeStamp" = CASE WHEN "voyDepartureYear" IS NOT NULL AND "voyDepartureMonth" IS NOT NULL AND "voyDepartureDay" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyDepartureDay", "voyDepartureMonth", "voyDepartureYear"),  'DD MM YYYY') END;
+UPDATE "bgbVoyageRoute"
+SET "voyArrTimeStamp" = CASE WHEN "voyArrivalYear" IS NOT NULL AND "voyArrivalMonth" IS NOT NULL AND "voyArrivalDay" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyArrivalDay", "voyArrivalMonth", "voyArrivalYear"),  'DD MM YYYY') END;
+
 -- TEMPORAL CONCATENATION
 
 -- UPDATE "bgbVoyageRoute" 
@@ -9,13 +14,5 @@ ALTER TABLE "bgbVoyageRoute"
 -- 	"voyDeparturePlaceNode" = "node"
 -- FROM "bgbPlaceGeo" AS geo
 -- WHERE "voyDeparturePlaceId" = geo.id;
-
-
--- 	CASE 
--- 		WHEN "voyArrivalYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyArrivalDay", "voyArrivalMonth", "voyArrivalYear"),  'DD MM YYYY')
--- 	END,
--- 	CASE 
--- 		WHEN "voyDepartureYear" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyDepartureDay", "voyDepartureMonth", "voyDepartureYear"),  'DD MM YYYY')
--- 	END
 
 
