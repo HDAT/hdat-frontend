c50148db9a1d50d08cdbc2bc67de7630e84496fc
evanzummeren <erikvanzummeren@gmail.com>
2015-04-19 21:47:41 +0200

    functie bijna af, extra locaties

--- src/scripts/build/sql/4-voyagesTime.sql
+++ src/scripts/build/sql/4-voyagesTime.sql
@@ -1,14 +1,33 @@
 ALTER TABLE "bgbVoyageRoute" 
 	ADD COLUMN "voyDepTimeStamp" timeStamp,
+	ADD COLUMN "voyDepTimeStampUnix" double precision,
 	ADD COLUMN "voyArrTimeStamp" timeStamp,
 	ADD COLUMN "speed" integer, -- km/h
-	ADD COLUMN "length" float; -- km
+	ADD COLUMN "length" float, -- km
+	ADD COLUMN "timeArray" json;
 
 UPDATE "bgbVoyageRoute" SET 
 	"voyDepTimeStamp" 	= CASE WHEN "voyDepartureYear" IS NOT NULL AND "voyDepartureMonth" IS NOT NULL AND "voyDepartureDay" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyDepartureDay", "voyDepartureMonth", "voyDepartureYear"),  'DD MM YYYY') END,
 	"voyArrTimeStamp" 	= CASE WHEN "voyArrivalYear" IS NOT NULL AND "voyArrivalMonth" IS NOT NULL AND "voyArrivalDay" IS NOT NULL THEN to_timestamp(CONCAT_WS(' ', "voyArrivalDay", "voyArrivalMonth", "voyArrivalYear"),  'DD MM YYYY') END,
-	"speed" 			= '10', --km/h
+	"speed" 			= '5', --km/h
 	"length" 			= (ST_Length_Spheroid("route",'SPHEROID["WGS84",6378137,298.257223563]')/1000); --km
+
+-- Generate missing departure times by using the arrival time
+UPDATE "bgbVoyageRoute" SET 
+	"voyDepTimeStamp"	= 	CASE WHEN "voyArrTimeStamp" IS NOT NULL AND "voyDepTimeStamp" IS NULL 
+								THEN ("voyArrTimeStamp"::timestamp - interval '1h' * ("length"/"speed"))
+							ELSE "voyDepTimeStamp" END;
+
+-- Convert to departure timestamp to unix 'timestamp'
+UPDATE "bgbVoyageRoute" SET 
+	"voyDepTimeStampUnix"	= (SELECT extract(epoch FROM "voyDepTimeStamp"));
+
+-- Create array around the generate time array function
+UPDATE "bgbVoyageRoute" SET
+	"timeArray"			= 	CASE WHEN "voyDepTimeStamp" IS NOT NULL
+								THEN (SELECT json_agg(times) FROM genTimeArray("voyDepTimeStamp","route","speed"))
+							END;
+
 
 -- TEMPORAL CONCATENATION
 
--- src/scripts/build/sql/functions/generateTimeArray.sql
+++ src/scripts/build/sql/functions/generateTimeArray.sql
@@ -5,3 +5,46 @@
 
 -- INPUT -> Depature time / Arrival time
 -- OUTPUT -> Timearray (sting or array, but should fit the single field that the voyage offers it)
+
+DROP FUNCTION genTimeArray(timestamp, geometry, integer);
+CREATE OR REPLACE FUNCTION genTimeArray(
+		IN voyDepTimeStamp timestamp,
+        IN route geometry(geometry, 4326),
+        IN speed integer,
+        OUT times timestamp
+    )
+    RETURNS SETOF timestamp AS
+$$
+
+DECLARE
+	iterator	integer 	:= 1;
+	points 		integer		:= (ST_NPoints(ST_setSRID($2,4326))+1);
+	time 		timestamp 	:= $1; 
+
+BEGIN
+	WHILE iterator < points
+	LOOP	
+		IF (iterator = 1) THEN
+			RETURN QUERY
+			SELECT  ($1);
+		ELSE
+			RETURN QUERY
+			SELECT 	(time + interval '1h' * 
+						((ST_Length_Spheroid(
+							(ST_Makeline(
+								ST_PointN(ST_setSRID($2,4326), iterator-1), 
+								ST_PointN(ST_setSRID($2,4326), (iterator))))
+						,'SPHEROID["WGS84",6378137,298.257223563]')/1000)/$3));
+			time := time + interval '1h' * ((ST_Length_Spheroid(
+						(ST_Makeline(
+							ST_PointN(ST_setSRID($2,4326), iterator-1), 
+							ST_PointN(ST_setSRID($2,4326), (iterator))))
+					,'SPHEROID["WGS84",6378137,298.257223563]')/1000)/$3);
+        END IF;
+        iterator := iterator + 1;
+
+    END LOOP;
+    RETURN;
+
+END;
+$$  LANGUAGE plpgsql;
