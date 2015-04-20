1af934a536d1b91f81b466c456b6f8a5d534033c
evanzummeren <erikvanzummeren@gmail.com>
2015-04-20 19:49:11 +0200

    fix json export

--- src/scripts/build/sql/export.sh
+++ src/scripts/build/sql/export.sh
@@ -1,4 +1,4 @@
-psql -d bgb -c "copy (SELECT row_to_json(t) FROM (select * from \"bgbVoyageRouteJSON\") t) to '/Users/$USER/Desktop/HDAT/src/data/json/voyages.json';"
+psql -d bgb -c "copy (select to_json(route::json) from \"bgbVoyageRouteJSON\") to '/Users/$USER/Desktop/HDAT/src/data/json/voyages.json';"
 
 awk '{filename = sprintf("/Users/'$USER'/Desktop/HDAT/src/data/json/split/voyage_%d.json", NR); print >filename; close(filename)}' /Users/$USER/Desktop/HDAT/src/data/json/voyages.json
 
