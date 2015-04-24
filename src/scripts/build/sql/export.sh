8d097109f4e2a87d7ec761700e467ddaf2be0825
Zaturrby <rjkorteschiel@gmail.com>
2015-04-24 21:02:34 +0200

    commented awk

--- src/scripts/build/sql/export.sh
+++ src/scripts/build/sql/export.sh
@@ -1,5 +1,5 @@
 psql -d bgb -c "copy (select to_json(route::json) from \"bgbVoyageRouteJSON\") to '/Users/$USER/Desktop/HDAT/src/data/json/voyages.json';"
 
-awk '{filename = sprintf("/Users/'$USER'/Desktop/HDAT/src/data/json/split/voyage_%d.json", NR); print >filename; close(filename)}' /Users/$USER/Desktop/HDAT/src/data/json/voyages.json
+# awk '{filename = sprintf("/Users/'$USER'/Desktop/HDAT/src/data/json/split/voyage_%d.json", NR); print >filename; close(filename)}' /Users/$USER/Desktop/HDAT/src/data/json/voyages.json
 
 # awk '{filename = sprintf("/Users/$USER/Desktop/HDAT/src/data/json/split/voyage_%d.json", NR); print >filename; close(filename)}' voyage.json
