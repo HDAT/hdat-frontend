dcb6703dccd0ae5cf701a5b29909d3f9069316b0
Zaturrby <rjkorteschiel@gmail.com>
2015-05-16 20:51:59 +0200

    Update 2015-05-16T18:49:34.492Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,66 +1,42 @@
 'use strict';
 
-var southWest   = L.latLng(-70, 179),
-    northEast   = L.latLng(70, -179),
+var southWest   = L.latLng(-75, 179),
+    northEast   = L.latLng(75, -179),
     bounds      = L.latLngBounds(southWest, northEast);
 
 var satellite   = L.tileLayer('https://{s}.tiles.mapbox.com/v4/erva.33c59435/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZXJ2YSIsImEiOiJUNy1GUV84In0.YTqElwgLmBOW8higJ-9GIw', {id: 'satellite'}),
     geography   = L.tileLayer('images/tilesoverlay/{z}/{x}/{y}.png', {id: 'geography'});
-
-/* Basiskaart */
-var baseMaps = {
-    "Satellite": satellite,
-};
-
-/* Optionele kaart, hier moet later routing network in */
-var overlayMaps = {
-    "Geography": geography
-};
   
 var map = new L.Map('map', {
   zoomControl:          true,
-  center:               [10, 35],
+  center:               [10, 45],
   minZoom:              3,
-  maxZoom:              5,
+  maxZoom:              6,
   zoom:                 3,
-  noWrap:               false,
   maxBounds:            bounds,
   attributionControl:   false,
   inertia:              true,
-  worldCopyJump:        true,
   layers:               [satellite, geography]
 });
 
-L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-L.Icon.Default.imagePath = 'images/leaflet/';
+// L.Icon.Default.imagePath = 'images/leaflet/';
+  
+var customIcon = L.icon({
+    iconUrl:                'images/hdat-shipicon.png',
+    className:              'hdat-shipicon',
+    iconSize:               [20, 20],   // size of the icon
+    iconAnchor:             [10, 10]   // icon center point
+});
 
 var data;           
-$(document).ready(function(){       
-    $.ajax({
-        type: 'GET',
-        url: 'data/json/voyages.json',
-        data: { get_param: 'value'},
-        dataType: 'json',
-        complete: function(gotdata){
-            data = gotdata;
-        }
-    });
-});
-
-var customIcon = L.icon({
-    iconUrl:                'images/leaflet/customIcon.png',
-    className:              'customIcon',
-    iconSize:               [20, 20],   // size of the icon
-    iconAnchor:             [10, 10],   // icon center point
-    popupAnchor:            [10, 10]    // point from which the popup should open relative to the iconAnchor
-});
-
-$(document).ajaxComplete(function(){            
-    data = $.parseJSON(data.responseText); //Takes AJAX Reponse Text and parses it to JSON
-    console.log(data[0].properties.time);
-
-    var playbackOptions = {
+$.ajax({
+    type: 'GET',
+    url: 'data/json/voyageshuygens.json',
+    data: { get_param: 'value'},
+    dataType: 'json',
+    complete: function(data){
+        data = $.parseJSON(data.responseText);
+        var playbackOptions = {
         playControl:            true,
         dateControl:            true,
         sliderControl:          true,
@@ -70,7 +46,7 @@ $(document).ajaxComplete(function(){
         marker:                 function(){
                                     return { icon: customIcon }      
                                 }     
-    };
-           
+      };
     var playback = new L.Playback(map, data, null, playbackOptions);
-}); 
+  }
+});
