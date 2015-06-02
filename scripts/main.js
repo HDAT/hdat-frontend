51b2e305a181791fb649b723c0214657bb9a9004
evanzummeren <erikvanzummeren@gmail.com>
2015-06-02 22:43:48 +0200

    Update 2015-06-02T20:39:24.067Z

--- scripts/main.js
+++ scripts/main.js
@@ -886,8 +886,8 @@ var southWest   = L.latLng(-75, 179),
     northEast   = L.latLng(75, -179),
     bounds      = L.latLngBounds(southWest, northEast);
 
-var satellite   = L.tileLayer('https://{s}.tiles.mapbox.com/v4/erva.33c59435/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZXJ2YSIsImEiOiJUNy1GUV84In0.YTqElwgLmBOW8higJ-9GIw', {id: 'satellite'}),
-    geography   = L.tileLayer('images/tilesoverlay2/{z}/{x}/{y}.png', {id: 'geography'});
+var satellite   = L.tileLayer('images/tiles/baselayer/{z}/{x}/{y}.png', {id: 'satellite'}),
+    geography   = L.tileLayer('images/tiles/overlay/{z}/{x}/{y}.png', {id: 'geography'});
   
 var map = new L.Map('map', {
   zoomControl:          true,
