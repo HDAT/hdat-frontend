b4ffb5b75c690f8f27837b563052119e15e8bb16
evanzummeren <erikvanzummeren@gmail.com>
2015-11-07 18:54:40 +0100

    animatie smooth

--- src/components/interface/Timeline.jsx
+++ src/components/interface/Timeline.jsx
@@ -14,7 +14,7 @@ class Timeline extends React.Component{
 			if (node !== undefined) {
 				var timeline = document.querySelector('.timeline-player'),
 					timeSlider = document.querySelector('.time-slider'),
-					year = document.querySelectorAll('.year');
+					year = document.querySelectorAll('.extra-years-container');
 
 
 				timeSlider.addEventListener("mouseover", function(){
@@ -24,7 +24,7 @@ class Timeline extends React.Component{
 							year[i].className += ' extra-years';
 						}
 						function addClassTimer() {
-				  			var blaat = window.setTimeout(addClass, 600);
+				  			window.setTimeout(addClass, 300);
 						}
 						addClassTimer();
 					}
@@ -32,7 +32,7 @@ class Timeline extends React.Component{
 				timeSlider.addEventListener("mouseout", function(){
 					timeline.className = 'timeline-player';
 					for(let i=0; i < year.length; i++) {
-						year[i].className = 'year';
+						year[i].className = 'extra-years-container';
 					}
 				})
 			}
@@ -46,16 +46,16 @@ class Timeline extends React.Component{
 			<div className="timeline">
 				<div className="timeline-player">
 					<ul className="year-container">
-						<li className="year hide-narrow"><span>1710</span></li>
-						<li className="year"><span>1720</span></li>
-						<li className="year hide-narrow"><span>1730</span></li>
-						<li className="year"><span>1740</span></li>
-						<li className="year hide-narrow"><span>1750</span></li>
-						<li className="year"><span>1760</span></li>
-						<li className="year hide-narrow"><span>1770</span></li>
-						<li className="year"><span>1780</span></li>
-						<li className="year hide-narrow"><span>1790</span></li>
-						<li className="year"><span>1800</span></li>
+						<li className="year hide-narrow"><div className="extra-years-container"></div><span>1710</span></li>
+						<li className="year"><div className="extra-years-container"></div><span>1720</span></li>
+						<li className="year hide-narrow"><div className="extra-years-container"></div><span>1730</span></li>
+						<li className="year"><div className="extra-years-container"></div><span>1740</span></li>
+						<li className="year hide-narrow"><div className="extra-years-container"></div><span>1750</span></li>
+						<li className="year"><div className="extra-years-container"></div><span>1760</span></li>
+						<li className="year hide-narrow"><div className="extra-years-container"></div><span>1770</span></li>
+						<li className="year"><div className="extra-years-container"></div><span>1780</span></li>
+						<li className="year hide-narrow"><div className="extra-years-container"></div><span>1790</span></li>
+						<li className="year"><div className="extra-years-container"></div><span>1800</span></li>
 					</ul>
 				</div>
 			</div>
