80f484ce936424749af90411d3246eec2406a1d9
evanzummeren <erikvanzummeren@gmail.com>
2015-11-11 13:54:35 +0100

    blaat

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,9 +2,6 @@ import React from "react";
 
 import LeafletContainer from "./map/LeafletContainer.jsx";
 import LeftMenu from "./interface/LeftMenu.jsx";
-  import Narratives from "./interface/Narratives.jsx"
-	import Share from "./interface/Share.jsx";
-	import Filter from "./interface/Filter.jsx";
 import Timeline from "./interface/Timeline.jsx";
 import EventHandler from "./interface/EventHandler.jsx";
 
