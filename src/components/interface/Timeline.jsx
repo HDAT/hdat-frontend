75b92cb768369d9f860c4b2d96c9c747f339813c
evanzummeren <erikvanzummeren@gmail.com>
2015-11-11 17:47:44 +0100

    refactored code: thanks @Zaturrbby

--- src/components/interface/Timeline.jsx
+++ src/components/interface/Timeline.jsx
@@ -3,39 +3,14 @@ import ReactDOM from 'react-dom';
 import ElementQueries from '../../../node_modules/css-element-queries/src/ElementQueries';
 import ResizeSensor from '../../../node_modules/css-element-queries/src/ResizeSensor';
 
-
 class Timeline extends React.Component{
-
-	timelineEnter() {
-		var timeline = document.querySelector('.timeline'),
-			timeSlider = document.querySelector('.time-slider'),
-			year = document.querySelectorAll('.decade-needles');
-
-		timeline.className += ' timeline-player-increased';
-		for(let i=0; i < year.length; i++) {
-			function addClass(){
-				year[i].className += ' extra-years';
-			}
-			function addClassTimer() {
-	  			window.setTimeout(addClass, 300);
-			}
-			addClassTimer();
-		}
+	timelineToggle() {
+		document.querySelector('.timeline').classList.toggle('timeline-triggered');
 	}
-
-	timelineLeave(){
-		var timeline = document.querySelector('.timeline');
-		var	year = document.querySelectorAll('.decade-needles');
-
-		timeline.className = 'timeline';
-		for(let i=0; i < year.length; i++) {
-			year[i].className = 'decade-needles';
-		}
-	}
-
 	render() {
+		window.timelineEnter = this.timelineEnter;
 		return(
-			<ul className="timeline" onMouseEnter={this.timelineEnter} onMouseLeave={this.timelineLeave}>
+			<ul className="timeline" onMouseEnter={this.timelineToggle} onMouseLeave={this.timelineToggle}>
 				{ [1710,1720,1730,1740,1750,1760,1770,1780,1790,1800].map(function(value, key){
 					return (
 						<li key={key} className="year">
