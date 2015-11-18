a8fd1fa5859141d8daf8e8dd5c69a3c730227d87
evanzummeren <erikvanzummeren@gmail.com>
2015-11-18 15:00:00 +0100

    small change to router

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -2,27 +2,35 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
 
-import "babel/polyfill";
-import "../styles/app.scss";
-import navGui from '../assets/data/cargoGuiNav.json';
-
 import App from "./app.jsx";
 import Narratives from "./interface/overlays/Narratives.jsx"
 import Share from "./interface/overlays/Share.jsx";
 import Filter from "./interface/overlays/Filter.jsx";
 
 class RouterComponent extends React.Component{
+	constructor(props){
+		super(props);
+		let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+		this.state = {
+			data: {
+				alphabet: alphabet
+			}
+		}
+	}
+
 	render() {
+		let alphabet = this.state.data.alphabet;
 		return (
 		  	<Router>
 		  		{/* https://github.com/rackt/react-router/issues/1531 */}
 		    	<Route path="/" component={App} >
 		    		<Route path="narrativelink" component={Narratives} />
 		        	<Route path="sharelink" component={Share} />
-		        	{ navGui.map(function(value, key){
-		        		return(<Route path={"filterlink/" + value.letter} component={Filter} filter={value.letter}/>)
-		        		}) 
-		        	}
+		        	<Route path="filterlink" component={Filter} >
+		        		{alphabet.map((letter, key)=>{
+		        			return <Route key="key" path={alphabet[letter]} filter={alphabet[letter]} />
+		        		})}
+		        	</Route>
 		    	</Route>
 		  	</Router>
 		)
