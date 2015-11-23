65a4bb26136f94d86aa4e52c80f4b22c84ed65c0
evanzummeren <erikvanzummeren@gmail.com>
2015-11-23 19:42:07 +0100

    rm ramda lodash

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ var config = {
     ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ]
