32c8cd09571f967bb4c768299bd2db1aea9a3550
Zaturrby <rjkorteschiel@gmail.com>
2015-12-12 13:51:59 +0100

    geojson fix

--- src/components/map/map-element.jsx
+++ src/components/map/map-element.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import Leaflet from 'leaflet';
 import { Map, TileLayer, Marker, LayerGroup, GeoJson } from 'react-leaflet';
 
+
 import MapWithVoyages from './playback/map-with-voyages.jsx';
 import minardData from '../../assets/data/map/minard/product-id-1000.json';
+
+class GeoJsonFix extends GeoJson {
+	static propTypes() {
+    	return { data: React.PropTypes.object.isRequired || React.PropTypes.array.isRequired}
+  	};
+}
 
 class MapElement extends React.Component{
     render() {
@@ -15,14 +22,18 @@ class MapElement extends React.Component{
 	    		minZoom={3} 
 	    		maxZoom={6} 
 	    		maxBounds={[[-75, 179],[75, -179]]}>
-	    		<GeoJson data={minardData} style={(feature)=>{
-		           	return {  
-		           		weight: feature.properties.cargovalues / 20000, 
-	                    color: '#2fcdfc', 
-	                    opacity: 0.2, 
-	                    lineJoin: 'miter',
-	                    lineCap: 'butt' }; 
-		            }}/>
+	    			<GeoJsonFix 
+	    				data={minardData} 
+	    				style={(feature, value)=>{
+	    					var value = 20000;
+	    					var key = 1;
+			           		return {  
+				           		weight: feature.properties.cargovalues / value, 
+			                    color: '#2fcdfc', 
+			                    opacity: 0.2, 
+			                    lineJoin: 'round',
+			                    lineCap: 'butt' }; 
+			          }}/>
 	    		<TileLayer url={'tiles/base/{z}/{x}/{y}.png'} />
 	    		<TileLayer url={'tiles/overlay/{z}/{x}/{y}.png'} />
 	  		</MapWithVoyages>
