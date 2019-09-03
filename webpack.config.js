815c62bd9346611e9de83fa60fa265206215ef63
Zaturrby <rjkorteschiel@gmail.com>
2019-09-03 18:21:57 +0200

    change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var webpack = require('webpack');
+var webpack = require('webpack')
 
 var config = {
   entry: {
@@ -7,33 +7,41 @@ var config = {
       'webpack/hot/only-dev-server',
       './src/entry.jsx'
     ],
-    vendor: [
-      'react'
-    ]
+    vendor: ['react']
   },
   devServer: {
     historyApiFallback: true
   },
   output: {
     path: './build',
-    publicPath: 'http://localhost:8080/',
-    filename: "bundle.js"
+    publicPath: '/',
+    filename: 'bundle.js'
   },
   devtool: 'eval-source-map',
   module: {
     loaders: [
-      { test: /\.(js|jsx)$/, loaders: ['react-hot', 'babel-loader'], exclude: /node_modules/},
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['react-hot', 'babel-loader'],
+        exclude: /node_modules/
+      },
       { test: /\.json$/, loader: 'json' },
-      { test: /\.scss$/, loader: 'style!css!sass!autoprefixer-loader?browsers=last 2 versions' },
-      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=1000' }
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass!autoprefixer-loader?browsers=last 2 versions'
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=1000'
+      }
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
-    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ]
-};
+}
 
-module.exports = config;
+module.exports = config
