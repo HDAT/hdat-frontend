abb29279a20962ae02e9baf8167084c1026f2fff
Zaturrby <rjkorteschiel@gmail.com>
2019-09-03 15:22:47 +0200

    everything works, sort of

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,29 +1,35 @@
-import React from "react";
+import React from 'react'
 
-import MapElement from "./map/map-element.jsx";
-import LeftMenu from "./interface/left-menu.jsx";
+import MapElement from './map/map-element.jsx'
+import LeftMenu from './interface/left-menu.jsx'
 
-class App extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			selectedProduct: 1
-		}
-	}
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedProduct: 1
+    }
+  }
 
-	changeProduct(selectedProduct){
-		this.setState({selectedProduct: selectedProduct}, this.props.history.pushState.bind(null, {}, '/'));
-	}
+  changeProduct(selectedProduct) {
+    this.setState(
+      { selectedProduct: selectedProduct },
+      this.props.history.pushState.bind(null, {}, '/')
+    )
+  }
 
   render() {
     return (
       <div className="app-container">
+        <MapElement selectedProduct={this.state.selectedProduct} />
         <LeftMenu />
-        {this.props.children && React.cloneElement(this.props.children, {changeProduct: this.changeProduct.bind(this)})}
-        <MapElement selectedProduct={this.state.selectedProduct} />  
+        {this.props.children &&
+          React.cloneElement(this.props.children, {
+            changeProduct: this.changeProduct.bind(this)
+          })}
       </div>
     )
   }
 }
 
-export default App;
+export default App
--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,33 +1,33 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute} from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, IndexRoute } from 'react-router'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 
-import App from "./app.jsx";
-import ShipViewer from "./interface/ship-viewer/ship-viewer.jsx"
-import GoodsUI from "./interface/goods/goods-ui.jsx";
-import MinardInfo from "./interface/minard/minard-info.jsx";
-import Graph from "./interface/graph/graph.jsx";
-import Events from "./interface/events/events.jsx";
-import Share from "./interface/share/share.jsx";
-import Timeline from "./interface/timeline/timeline.jsx";
+import App from './app.jsx'
+import ShipViewer from './interface/ship-viewer/ship-viewer.jsx'
+import GoodsUI from './interface/goods/goods-ui.jsx'
+import MinardInfo from './interface/minard/minard-info.jsx'
+import Graph from './interface/graph/graph.jsx'
+import Events from './interface/events/events.jsx'
+import Share from './interface/share/share.jsx'
+import Timeline from './interface/timeline/timeline.jsx'
 
-class RouterComponent extends React.Component{
-	render() {
-		return (
-	  	<Router history={createBrowserHistory()}>
-	    	<Route path="/" component={App}>
-	    		<IndexRoute component={Timeline} />
-	    		<Route path="/ships" component={ShipViewer} />
-	        <Route path="/goods/:letter" component={GoodsUI} />
-	    		<Route path="/minard" component={MinardInfo} />
-	    		<Route path="/graph" component={Graph} />
-	    		<Route path="/events" component={Events} />
-	        <Route path="/share" component={Share} />
-	    	</Route>
-	  	</Router>
-		);
-	}
+class RouterComponent extends React.Component {
+  render() {
+    return (
+      <Router history={createBrowserHistory()}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Timeline} />
+          <Route path="/ships" component={ShipViewer} />
+          <Route path="/goods/:letter" component={GoodsUI} />
+          <Route path="/minard" component={MinardInfo} />
+          <Route path="/graph" component={Graph} />
+          <Route path="/events" component={Events} />
+          <Route path="/share" component={Share} />
+        </Route>
+      </Router>
+    )
+  }
 }
 
-export default RouterComponent;
+export default RouterComponent
